aa36461b1d1a268b2c58a81fecb862dde34d5623
MichaelKTHP <michaelkrasnovsky@gmail.com>
2019-11-04 17:40:01 +0100

    fin projet lundi

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,10 +5,23 @@ class UserMailer < ApplicationMailer
     #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
     @user = user 
 
-    #on définit une variable @url qu'on utilisera dans la view d’e-mail
+    #on définit  variable @url qu'on utilisera dans la view d’e-mail
     @url  = 'http://monsite.fr/login' 
 
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
     mail(to: @user.email, subject: 'Bienvenue chez nous !') 
   end
+
+  def attend_welcome_email(attendance)
+
+  	 #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
+    @attendance = attendance
+
+    #on définit  variable @url qu'on utilisera dans la view d’e-mail
+    @url  = 'http://monsite.fr/event' 
+
+    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+    mail(to: @attendance.attendee.email, subject: 'Your participation was registered !')  
+  end
+
 end
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,10 @@
 class Attendance < ApplicationRecord
+after_create :welcome_send
+
+ def welcome_send
+    UserMailer.attend_welcome_email(self).deliver_now
+  end
+
 belongs_to :attendee, class_name: "User"
 belongs_to :event
 end
