eec3b969dbc0b69b288a5ccaa69d2f6009a3ddae
MichaelKTHP <michaelkrasnovsky@gmail.com>
2019-11-08 13:19:18 +0100

    modif picture#is_admin

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -14,7 +14,7 @@ before_action :is_admin, only: [:create]
      private
 
       def is_admin
-  	    @event = Event.find(params[:id])
+  	    @event = Event.find(params[:event_id])
   	      if current_user != @event.admin
   		   flash[:error] = "Vous n'êtes pas le créateur de cet évenement !"
   		   redirect_to event_path(@event.id)
